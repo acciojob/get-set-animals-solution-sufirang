03878843dbb850f74be26bcc1c3dad3a4c94f5cd
Sufia Bano <124654106+sufirang@users.noreply.github.com>
2023-04-09 20:44:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 class Animal {
   constructor(species) {
-    this._species = species;
+    this.species = species;
   }
 
   get species() {
-    return this._species;
+    return this.species;
   }
 
   makeSound() {
